81a558b941f2bb0c95437f8277917e1fd4920457
Tim van Ekert <tim.van.ekert@hva.nl>
2021-02-16 22:07:06 +0100

    addition of scanner

---
+++ A2/direct-coded-scanner.cpp
@@ -0,0 +1,40 @@
+/**
+ * 
+ *  Scanner for regex (ac|ab)*
+ * 
+ */
+char *scanner( char *stream)
+{
+    int pos = 0; char c;
+
+// scan and check for character a
+state_init:
+    c = stream[pos++];
+    if (c == 'a') goto state_1; 
+    if (pos == 1) goto state_err;
+    else goto state_done;
+
+// scan and check for character b or c
+state_1:
+    c = stream[pos++];
+    if (c == 'c' || c == 'b') goto state_0;
+    else goto state_err;
+
+// scan and check for character a
+state_0:
+    c = stream[pos++];
+    if (c == 'a') goto state_1; 
+    else goto state_done;
+
+// state is done and return state success
+state_done:
+    goto state_succ; 
+
+// state success return stream
+state_succ:
+    return stream;
+
+// state error so return null
+state_err:
+    return NULL;
+}
