0961e5ee5717bb32715db094cbb3fd974a594072
Tim van Ekert <tim.van.ekert@hva.nl>
2021-02-17 16:51:19 +0100

    addition of final report

--- A2/direct-coded-scanner.cpp
+++ A2/direct-coded-scanner.cpp
@@ -3,32 +3,36 @@
  *  Scanner for regex (ac|ab)*
  * 
  */
-char *scanner( char *stream)
+char *scanner(char *stream)
 {
-    int pos = 0; char c;
+    int pos = 0;
+    char c;
 
 // scan and check for character a
 state_init:
     c = stream[pos++];
-    if (c == 'a') goto state_1; 
-    if (pos == 1) goto state_err;
-    else goto state_done;
-
-// scan and check for character b or c
-state_1:
-    c = stream[pos++];
-    if (c == 'c' || c == 'b') goto state_0;
-    else goto state_err;
+    if (c == 'a')
+        goto state_1;
+    if (pos == 1)
+        goto state_err;
+    else
+        goto state_succ;
 
 // scan and check for character a
 state_0:
     c = stream[pos++];
-    if (c == 'a') goto state_1; 
-    else goto state_done;
+    if (c == 'a')
+        goto state_1;
+    else
+        goto state_succ;
 
-// state is done and return state success
-state_done:
-    goto state_succ; 
+// scan and check for character b or c
+state_1:
+    c = stream[pos++];
+    if (c == 'c' || c == 'b')
+        goto state_0;
+    else
+        goto state_err;
 
 // state success return stream
 state_succ:
