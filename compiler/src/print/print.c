997671da3192ff5e597e82fe909ce2c49db29cdb
Tim van Ekert <tim.van.ekert@hva.nl>
2021-02-23 22:41:39 +0100

    addition of prints in print.c

--- compiler/src/print/print.c
+++ compiler/src/print/print.c
@@ -70,7 +70,10 @@ node *
 PRTprogram (node * arg_node, info * arg_info)
 {
   DBUG_ENTER ("PRTprogram");
+  DBUG_PRINT ("PRT", ("PRTprogram"));
   
+  PROGRAM_DECLS( arg_node) = TRAVdo(PROGRAM_DECLS(arg_node), arg_info);
+
   DBUG_RETURN (arg_node);
 }
 
@@ -91,7 +94,11 @@ node *
 PRTdecls (node * arg_node, info * arg_info)
 {
   DBUG_ENTER ("PRTdecls");
-  
+  DBUG_PRINT ("PRT", ("PRTdecls"));
+
+  DECLS_DECL(arg_node) = TRAVdo(DECLS_DECL(arg_node), arg_info);
+  DECLS_NEXT(arg_node) = TRAVopt(DECLS_NEXT(arg_node), arg_info);
+
   DBUG_RETURN (arg_node);
 }
 
@@ -133,6 +140,15 @@ node *
 PRTexprs (node * arg_node, info * arg_info)
 {
   DBUG_ENTER ("PRTexprs");
+  DBUG_PRINT ("PRT", ("PRTexprs"));
+
+  EXPRS_EXPR( arg_node) = TRAVdo( EXPRS_EXPR( arg_node), arg_info);
+
+  if (EXPRS_NEXT( arg_node))
+  {
+    printf(", ");
+    EXPRS_NEXT( arg_node) = TRAVopt( EXPRS_NEXT( arg_node), arg_info); 
+  }
   
   DBUG_RETURN (arg_node);
 }
@@ -154,6 +170,9 @@ node *
 PRTarrexpr (node * arg_node, info * arg_info)
 {
   DBUG_ENTER ("PRTarrexpr");
+  DBUG_PRINT ("PRT", ("PRTarrexpr"));
+
+  ARREXPR_EXPRS(arg_node) = TRAVdo(ARREXPR_EXPRS(arg_node), arg_info);
   
   DBUG_RETURN (arg_node);
 }
@@ -175,7 +194,10 @@ node *
 PRTids (node * arg_node, info * arg_info)
 {
   DBUG_ENTER ("PRTids");
-  
+  DBUG_PRINT ("PRT", ("PRTarrexpr"));
+
+  printf("%s", IDS_NAME(arg_node));
+
   DBUG_RETURN (arg_node);
 }
 
@@ -196,6 +218,23 @@ node *
 PRTmonop (node * arg_node, info * arg_info)
 {
   DBUG_ENTER ("PRTmonop");
+  DBUG_PRINT ("PRT", ("PRTmonop"));
+  char *tmp;
+  
+  switch (MONOP_OP( arg_node)) {
+    case MO_minus:
+      tmp = "-";
+      break;
+    case MO_neg:
+      tmp = "!";
+      break;
+    case MO_unknown:
+      DBUG_ASSERT( 0, "unknown monop detected!");
+  }
+
+  printf( "%s", tmp);
+
+  MONOP_OPERAND( arg_node) = TRAVdo( MONOP_OPERAND( arg_node), arg_info);
   
   DBUG_RETURN (arg_node);
 }
@@ -217,7 +256,12 @@ node *
 PRTexprstmt (node * arg_node, info * arg_info)
 {
   DBUG_ENTER ("PRTexprstmt");
+  DBUG_PRINT ("PRT", ("PRTexprstmt"));
   
+  EXPRSTMT_EXPR(arg_node) = TRAVdo(EXPRSTMT_EXPR(arg_node), arg_info);
+
+  printf(";\n");
+
   DBUG_RETURN (arg_node);
 }
 
@@ -238,7 +282,19 @@ node *
 PRTreturn (node * arg_node, info * arg_info)
 {
   DBUG_ENTER ("PRTreturn");
-  
+  DBUG_PRINT ("PRT", ("PRTreturn"));
+
+  printf("%s", "return");
+
+  if(RETURN_EXPR(arg_node))
+  {
+    printf(" ");
+    RETURN_EXPR( arg_node) = TRAVopt( RETURN_EXPR( arg_node), arg_info);
+  }
+
+  // print a semicolon
+  printf(";\n");
+
   DBUG_RETURN (arg_node);
 }
 
@@ -259,7 +315,14 @@ node *
 PRTfuncall (node * arg_node, info * arg_info)
 {
   DBUG_ENTER ("PRTfuncall");
-  
+  DBUG_PRINT ("PRT", ("PRTfuncall"));
+
+  printf("%s(", FUNCALL_NAME(arg_node));
+
+  FUNCALL_ARGS(arg_node) = TRAVopt(FUNCALL_ARGS(arg_node), arg_info);
+
+  printf(")");
+
   DBUG_RETURN (arg_node);
 }
 
@@ -280,6 +343,11 @@ node *
 PRTcast (node * arg_node, info * arg_info)
 {
   DBUG_ENTER ("PRTcast");
+  DBUG_PRINT ("PRT", ("PRTcast"));
+
+  printf("( %s )", typeInString(CAST_TYPE(arg_node)));
+
+  CAST_EXPR(arg_node) = TRAVdo(CAST_EXPR(arg_node), arg_info);
   
   DBUG_RETURN (arg_node);
 }
@@ -301,7 +369,11 @@ node *
 PRTfundefs (node * arg_node, info * arg_info)
 {
   DBUG_ENTER ("PRTfundefs");
+  DBUG_PRINT ("PRT", ("PRTfundefs"));
   
+  FUNDEFS_FUNDEF(arg_node) = TRAVdo(FUNDEFS_FUNDEF(arg_node), arg_info);
+  FUNDEFS_NEXT(arg_node) = TRAVopt(FUNDEFS_FUNDEF(arg_node), arg_info);
+
   DBUG_RETURN (arg_node);
 }
 
@@ -322,6 +394,34 @@ node *
 PRTfundef (node * arg_node, info * arg_info)
 {
   DBUG_ENTER ("PRTfundef");
+  DBUG_PRINT ("PRT", ("PRTfundef"));
+
+  if (FUNDEF_ISEXTERN(arg_node))
+  {
+    printf("%s", "extern");
+  } else if (FUNDEF_ISEXPORT(arg_node)) {
+    printf("%s", "export");
+  }
+
+  printf("%s %s", typeInString(FUNDEF_TYPE (arg_node)), FUNDEF_NAME (arg_node));
+  printf(" ( ");
+  
+  FUNDEF_PARAMS(arg_node) = TRAVopt(FUNDEF_PARAMS(arg_node), arg_info);
+  printf(" ) ");
+
+  if (FUNDEF_FUNBODY (arg_node) != NULL)
+  {
+    printf("\n{\n");
+    // INFO_TABS(arg_info) += 1;
+
+    FUNDEF_FUNBODY(arg_node) = TRAVopt( FUNDEF_FUNBODY(arg_node), arg_info);
+
+    // INFO_TABS(arg_info) -= 1;
+    printf("}\n");
+  } else {
+    printf(";\n");
+  }
+  
   
   DBUG_RETURN (arg_node);
 }
@@ -343,6 +443,11 @@ node *
 PRTfunbody (node * arg_node, info * arg_info)
 {
   DBUG_ENTER ("PRTfunbody");
+  DBUG_PRINT ("PRT", ("PRTfunbody"));
+
+  FUNBODY_VARDECLS(arg_node) = TRAVopt(FUNBODY_VARDECLS(arg_node), arg_info);
+  FUNBODY_LOCALFUNDEFS( arg_node) = TRAVopt( FUNBODY_LOCALFUNDEFS( arg_node), arg_info);
+  FUNBODY_STMTS( arg_node) = TRAVopt( FUNBODY_STMTS( arg_node), arg_info);
   
   DBUG_RETURN (arg_node);
 }
@@ -364,7 +469,28 @@ node *
 PRTifelse (node * arg_node, info * arg_info)
 {
   DBUG_ENTER ("PRTifelse");
-  
+  DBUG_PRINT ("PRT", ("PRTifelse"));
+
+  printf("if ( ");
+  IFELSE_COND( arg_node) = TRAVdo( IFELSE_COND( arg_node), arg_info);
+  printf(" )\n");
+  printf("{\n");
+  // INFO_TABS ( arg_info) += 1;
+  IFELSE_THEN( arg_node) = TRAVopt( IFELSE_THEN( arg_node), arg_info);
+  // INFO_TABS ( arg_info) -= 1;
+
+  printf("}\n");
+
+  if (IFELSE_ELSE( arg_node))
+  {
+    printf("else\n");
+    printf("{\n");
+    // INFO_TABS ( arg_info) += 1;
+    IFELSE_ELSE( arg_node) = TRAVopt( IFELSE_ELSE( arg_node), arg_info);
+    // INFO_TABS ( arg_info) -= 1;
+    printf("}\n");
+  }
+
   DBUG_RETURN (arg_node);
 }
 
