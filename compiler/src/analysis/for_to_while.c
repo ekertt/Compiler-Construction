ba5d48ac58fbeef4ad9dd284a2aac83d271b2aef
Tim van Ekert <tim.van.ekert@hva.nl>
2021-03-25 21:53:35 +0100

    refactored some

--- compiler/src/analysis/for_to_while.c
+++ compiler/src/analysis/for_to_while.c
@@ -1,5 +1,4 @@
 #include "for_to_while.h"
-#include "linked_list_KV.h"
 
 #include "types.h"
 #include "tree_basic.h"
@@ -11,6 +10,130 @@
 #include "str.h"
 #include "copy.h"
 #include "ctinfo.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// used the key value linked list from: https://www.tutorialspoint.com/data_structures_algorithms/linked_list_program_in_c.htm
+/*
+*
+*   This is a key value linkedlist helper to function for the for to while part of the code analysis
+*
+*/
+
+typedef struct keyvalueListNode
+{
+    const char *key;
+    const char *value;
+    struct keyvalueListNode *next;
+} keyvalueListNode;
+
+typedef void (*callback)(keyvalueListNode *data);
+
+keyvalueListNode *KeyValueCreateList(const char *key, const char *value, keyvalueListNode *next)
+{
+    keyvalueListNode *new_list_node = (keyvalueListNode *)malloc(sizeof(keyvalueListNode));
+    new_list_node->key = key;
+    new_list_node->value = value;
+    new_list_node->next = next;
+
+    return new_list_node;
+}
+
+keyvalueListNode *KeyValueDeleteFirst(const char *key, const char *value, keyvalueListNode *head)
+{
+    keyvalueListNode *new_keyvalueListNode = KeyValueCreateList(key, value, head);
+    head = new_keyvalueListNode;
+    return head;
+}
+
+keyvalueListNode *KeyValueFrontOfList(keyvalueListNode *head)
+{
+    if (head == NULL)
+    {
+        return NULL;
+    }
+    keyvalueListNode *current = head;
+    head = head->next;
+    current->next = NULL;
+    if (current == head)
+    {
+        head = NULL;
+    }
+    free(current);
+
+    return head;
+}
+
+keyvalueListNode *KeyValueRemoveLast(keyvalueListNode *node)
+{
+
+    keyvalueListNode *current = node;
+    keyvalueListNode *back = NULL;
+
+    if (node == NULL)
+        return NULL;
+
+    while (current->next != NULL)
+    {
+        back = current;
+        current = current->next;
+    }
+
+    if (back != NULL)
+    {
+        back->next = NULL;
+    }
+
+    if (current == node)
+    {
+        node = NULL;
+    }
+
+    free(current);
+
+    return node;
+}
+
+keyvalueListNode *KeyValueSearch(keyvalueListNode *head, const char *key)
+{
+
+    keyvalueListNode *current = head;
+    while (current != NULL)
+    {
+        if (strcmp(current->key, key) == 0)
+        {
+            return current;
+        }
+
+        current = current->next;
+    }
+    return NULL;
+}
+
+void KeyValueDispose(keyvalueListNode *head)
+{
+    keyvalueListNode *current, *temp;
+
+    if (head != NULL)
+    {
+        current = head->next;
+        head->next = NULL;
+        while (current != NULL)
+        {
+            temp = current->next;
+            free(current);
+            current = temp;
+        }
+    }
+}
+
+/*
+*
+*   End of linked list
+*
+*/
 
 struct INFO
 {
@@ -52,33 +175,33 @@ static info *FreeInfo(info *info)
     DBUG_RETURN(info);
 }
 
-void append(node *front, node *new)
+void append(node *head, node *new)
 {
-    if (front == NULL)
+    if (head == NULL)
     {
         return;
     }
 
-    if (NODE_TYPE(front) == N_stmts)
+    if (NODE_TYPE(head) == N_stmts)
     {
-        if (STMTS_NEXT(front) == NULL)
+        if (STMTS_NEXT(head) == NULL)
         {
-            STMTS_NEXT(front) = new;
+            STMTS_NEXT(head) = new;
         }
         else
         {
-            append(STMTS_NEXT(front), new);
+            append(STMTS_NEXT(head), new);
         }
     }
-    else if (NODE_TYPE(front) == N_vardecl)
+    else if (NODE_TYPE(head) == N_vardecl)
     {
-        if (VARDECL_NEXT(front) == NULL)
+        if (VARDECL_NEXT(head) == NULL)
         {
-            VARDECL_NEXT(front) = new;
+            VARDECL_NEXT(head) = new;
         }
         else
         {
-            append(VARDECL_NEXT(front), new);
+            append(VARDECL_NEXT(head), new);
         }
     }
 }
