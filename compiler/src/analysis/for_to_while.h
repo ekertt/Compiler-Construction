c88088dc12c52eb539d47d8f90d0c39f4c507901
Tim van Ekert <tim.van.ekert@hva.nl>
2021-03-25 17:07:06 +0100

    fixed some more things

--- compiler/src/analysis/for_to_while.h
+++ compiler/src/analysis/for_to_while.h
@@ -2,7 +2,7 @@
 #define _FOR_TO_WHILE_H_
 #include "types.h"
 
-extern node *FTWdoNormalizeForLoops(node *syntaxtree);
+extern node *FTWdoForToWhile(node *syntaxtree);
 
 extern node *FTWfor (node * arg_node, info * arg_info);
 extern node *FTWfunbody (node * arg_node, info * arg_info);
